56375a310b2e964ca9868d8b7686e50b311c84c2
Shakhemir <shakhemir@gmail.com>
2022-06-27 19:22:28 +0300

    Добавил в логи дату со временем

--- logging_todo.py
+++ logging_todo.py
@@ -1,6 +1,7 @@
 from config import log_file
+from datetime import datetime
 
 
 def add_log(text):
     with open(log_file, 'a') as file:
-        print(text, file=file)
+        print(f'{datetime.now().strftime("%Y.%m.%d %H:%M:%S")} {text}', file=file)
